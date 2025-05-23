da7eb76be8b078b131690ddb9d36f515b4c0a8be
devi <126806486+debby019@users.noreply.github.com>
2025-05-23 05:06:54 -0700

    Add files via upload

--- backend/logic/user.py
+++ backend/logic/user.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException
 from passlib.hash import bcrypt
 from backend.conexion.dataBase import supabase
 from backend.models import UserCreate
+import uuid
 
 
 def get_user_by_email(email: str):
@@ -22,6 +23,22 @@ def create_user(user: UserCreate):
     
     return result.data[0]
 
+def create_guest_user():
+    guest_id = str(uuid.uuid4())
+    result = supabase.table("usuarios").insert({
+        "id_usuario": guest_id,
+        "nombre": f"invitado_{guest_id[:8]}",
+        "email": f"{guest_id}@guest.local",    # Email falso
+        "password": "",                        # campo requerido, aunque no lo usaremos
+        "rol": "guest",
+    }).execute()
+
+    if not result.data:
+        raise HTTPException(status_code=500, detail="No se pudo crear el usuario invitado.")
+    
+    return guest_id
+
+
 def verify_password(plain_password: str, hashed_password: str):
     return bcrypt.verify(plain_password, hashed_password)
 
@@ -37,5 +54,5 @@ def get_chats_by_user_id(user_id: str):
 
 def delete_user(user_id: str):
 
-    supabase.table("usuario").delete().eq("id_usuario", user_id).execute()
+    supabase.table("usuarios").delete().eq("id_usuario", user_id).execute()
     
